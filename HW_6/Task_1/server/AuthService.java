724a0772a6be516d42bd0cf481ec600054dae910
pamo4alov <49147631+pamo4alov@users.noreply.github.com>
2019-08-20 19:55:24 +0700

    Delete AuthService.java

--- HW_6/Task_1/server/AuthService.java
+++
@@ -1,8 +0,0 @@
-package HW_6.Task_1.server;
-
-public interface AuthService {
-
-    String getNickname(String login, String password);
-
-    boolean changeNickname(String currentNickname, String newNickname);
-}
